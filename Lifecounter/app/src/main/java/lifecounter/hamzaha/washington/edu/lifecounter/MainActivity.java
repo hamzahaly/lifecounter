18c937fd5de0be9f9eab5866673363154244d740
hamzahaly <hzhaly93@gmail.com>
2017-01-24 16:42:55 -0800

    starter

---
+++ Lifecounter/app/src/main/java/lifecounter/hamzaha/washington/edu/lifecounter/MainActivity.java
@@ -0,0 +1,13 @@
+package lifecounter.hamzaha.washington.edu.lifecounter;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

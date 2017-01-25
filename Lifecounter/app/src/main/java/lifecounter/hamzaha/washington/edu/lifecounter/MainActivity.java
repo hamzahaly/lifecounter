4fe4d820ffc47ec4fe3b22171b4562a1aedddbcf
hamzahaly <hzhaly93@gmail.com>
2017-01-24 18:38:43 -0800

    centers life

--- Lifecounter/app/src/main/java/lifecounter/hamzaha/washington/edu/lifecounter/MainActivity.java
+++ Lifecounter/app/src/main/java/lifecounter/hamzaha/washington/edu/lifecounter/MainActivity.java
@@ -2,6 +2,9 @@ package lifecounter.hamzaha.washington.edu.lifecounter;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +12,36 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MyListener myListener = new MyListener();
     }
+
+    private class MyListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View view) {
+            Button button = (Button) view.get();
+
+        }
+    }
+
+    //add one to life counter
+    public void plusOne(View view) {
+
+    }
+
+    //add five to life counter
+    public void plusFive() {
+
+    }
+
+    //minus one life counter
+    public void minusOne() {
+
+    }
+
+    //minus five to life counter
+    public void minusFive() {
+
+    }
+
 }
